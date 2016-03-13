327c7b6f8bf1921f1edc07d9147368990312f3cb
dust <du5t@multiplexed.be>
2016-03-12 18:53:38 -0800

    moved old experiment files

--- old/index.js
+++ old/index.js
@@ -1,42 +1,59 @@
 (function() {
 var getSelectedHTML = function() {
   var range = window.getSelection().getRangeAt(0),
-      content = range.extractContents(),
-      span = document.createElement('SPAN');
-
-  var modify = [];
+      content = range.cloneContents(),
+      span = document.createElement('span');
+  
   span.appendChild(content);
-  /* span.querySelectorAll('img').map(function (i, el) { modify.push(el) }); */
+  span.className = 'journalet-selected-content';
+  var modify = [];
+  /* iframe.querySelectorAll('img').map(function (i, el) { modify.push(el) }); */
 
-  return span.innerHTML;
+  return span;
+};
 
+var closeFloatyWindow = function() {
+  document.getElementById('floaty-window').remove();
+};
+
+var sendHTML = function() {
+  closeFloatyWindow();
 };
 
 var createFloatyWindow = function() {
   var menu = `<style>
  .journalet-menu {
+   z-index: 999;
    position: fixed;
    top: 0;
    right: 0;
    max-width: 50%;
    min-width: 10rem;
-   /* height: 25%; */
+   max-height: 600px;
    background: #222;
    /* border-width: 2px; */
-   border-color: cornsilk;
+   border-color: cornsilk !important;
    border-radius: 5px;
  }
 
  .journalet-selected-content {
+   position: relative;
    width: 100%;
    /* overflow-wrap: break-word; */
    overflow: scroll;
-   background-color: #CCC;
-   /* height: 40%; */
-   min-height: 40%;
+   background-color: inherit;
+   max-height: 400px;
+   height: 40%;
  }
 
  .journalet-selected-content {}
+
+ .journalet-hrule {
+   background-color: #EEE;
+   color: #EEE;
+   height:1px;
+   border:none;
+ }
 
  .journalet-title {
    color: #EEE;
@@ -75,21 +92,22 @@ var createFloatyWindow = function() {
  }
 </style>
 <div class="journalet-menu">
-  <div class="journalet-title">Title</div>
+  <div class="journalet-title">Capsule</div>
   <div class="journalet-selected-content">
-    ${getSelectedHTML()}
+    ${getSelectedHTML().outerHTML}
   </div>
-  <hr/>
+  <hr class="journalet-hrule" />
   <div class="journalet-comment-field">
     Comments go here.
   </div>
   <div class="journalet-controls">
-    <button>Cancel</button>
-    <button>Send</button>
+    <button onClick=${closeFloatyWindow}>Cancel</button>
+    <button onClick=${sendHTML}>Send</button>
   </div>
 </div>
 `;
   var insert = document.createElement('div');
+  insert.id = 'floaty-window';
   insert.innerHTML = menu;
   document.body.appendChild(insert);
 };
