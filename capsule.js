c7751db8e9c4b59e39c75a4831eae118a6bf0fe6
dust <du5t@multiplexed.be>
2016-03-13 17:56:37 -0700

    a stab at incorporating ssbify into browser

--- capsule.js
+++ capsule.js
@@ -1,78 +1,13 @@
-//Making & processing snippets
-
-// function processSnapshot() {
-// 	if (!lastSnapshot) {
-// 		return;
-// 	}
-// 
-// 	var styles = lastSnapshot.css,
-// 			html = lastSnapshot.html,
-// 			prefix = "";
-// 
-// 	if (includeAncestors.is(':checked')) {
-// 		styles = lastSnapshot.ancestorCss.concat(styles);
-// 		html = lastSnapshot.leadingAncestorHtml + html + lastSnapshot.trailingAncestorHtml;
-// 	}
-// 
-// 	loader.addClass('processing');
-// 
-// 	if (removeDefaultValuesInput.is(':checked')) {
-// 		styles = defaultValueFilter.process(styles);
-// 	}
-// 
-// 	if (propertiesCleanUpInput.is(':checked')) {
-// 		styles = shorthandPropertyFilter.process(styles);
-// 	}
-// 	if (removeWebkitPropertiesInput.is(':checked')) {
-// 		styles = webkitPropertiesFilter.process(styles);
-// 	}
-// 	if (combineSameRulesInput.is(':checked')) {
-// 		styles = sameRulesCombiner.process(styles);
-// 	}
-// 
-// 	if (fixHTMLIndentationInput.is(':checked')) {
-// 		html = $.htmlClean(html, {
-// 			removeAttrs: ['class'],
-// 			allowedAttributes: [
-// 				['id'],
-// 				['placeholder', ['input', 'textarea']],
-// 				['disabled', ['input', 'textarea', 'select', 'option', 'button']],
-// 				['value', ['input', 'button']],
-// 				['readonly', ['input', 'textarea', 'option']],
-// 				['label', ['option']],
-// 				['selected', ['option']],
-// 				['checked', ['input']]
-// 			],
-// 			format: true,
-// 			replace: [],
-// 			replaceStyles: [],
-// 			allowComments: true
-// 		});
-// 	}
-// 
-// 	styles = cssStringifier.process(styles);
-// 
-// 	if (isValidPrefix(idPrefix.val())) {
-// 		prefix = idPrefix.val();
-// 	}
-// 
-// 	//replacing prefix placeholder used in all IDs with actual prefix
-// 	html = html.replace(/:snappysnippet_prefix:/g, prefix);
-// 	styles = styles.replace(/:snappysnippet_prefix:/g, prefix);
-// 
-// 	htmlTextarea.val(html);
-// 	cssTextarea.val(styles);
-// 
-// 	loader.removeClass('processing');
-// }
-
+// var ssbClient = require('ssb-client')
+// var ssbify = require('./lib/ssbify-browser.js')
 
 var getSelectedHTML = function() {
   chrome.tabs.query({active: true}, function(tabs) {
     var tab = tabs[0]
+    debugger
     chrome.tabs.executeScript(
       {
-        file: 'serializeSelectedHTML.js',
+        file: './serializeSelectedHTML.js',
         runAt: 'document_end'
       },
       renderSelectedHTML
@@ -99,9 +34,13 @@ var renderSelectedHTML = function(selection) {
   }
 }
 
-var sendHTML = function() {
-  //  closeFloatyWindow()
-  window.close()
+var sendHTML = function(htmlString, url) {
+  ssbify(htmlString, { url: url, ignoreBrokenImgLinks: true },
+         function (err, res) {
+           if (err) throw err
+           console.log(res)
+           alert(res)
+         })
 }
 
 document.addEventListener('DOMContentLoaded', getSelectedHTML)
